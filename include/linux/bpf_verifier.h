fae7dfe599aadd411017c078b219be4a30b34ecb
Toke Høiland-Jørgensen <toke@redhat.com>
2021-04-13 11:16:06 +0200

    bpf: Return target info when a tracing bpf_link is queried
    There is currently no way to discover the target of a tracing program
    attachment after the fact. Add this information to bpf_link_info and return
    it when querying the bpf_link fd.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20210413091607.58945-1-toke@redhat.com

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -506,6 +506,15 @@ static inline u64 bpf_trampoline_compute_key(const struct bpf_prog *tgt_prog,
         return tgt_prog ? (((u64)tgt_prog->aux->id) << 32 | btf_id) : btf_id;
 }
 
+/* unpack the IDs from the key as constructed above */
+static inline void bpf_trampoline_unpack_key(u64 key, u32 *obj_id, u32 *btf_id)
+{
+	if (obj_id)
+		*obj_id = key >> 32;
+	if (btf_id)
+		*btf_id = key & 0x7FFFFFFF;
+}
+
 int bpf_check_attach_target(struct bpf_verifier_log *log,
 			    const struct bpf_prog *prog,
 			    const struct bpf_prog *tgt_prog,
